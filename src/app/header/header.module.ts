af2aba338ac9cb3280ffd04f6a5dcdb2c557b24d
PhantomD <klinger.steven.contact@gmail.com>
2018-01-11 23:10:17 +0100

    Welcome page on. V0.1

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { HeaderComponent } from './header.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatMenuItem } from '@angular/material/menu/typings/menu-item';
-import { MatIconModule } from '@angular/material/icon'
-import { MatMenuTrigger } from '@angular/material/menu/typings/menu-trigger';
 
 @NgModule({
   declarations: [
-    MatMenuTrigger
   ],
   imports: [  
-      MatMenuModule,
-      MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
